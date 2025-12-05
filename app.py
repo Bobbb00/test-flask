caefec1d0d2cfa4068d04ff654c21d0c24c4f2a2
Syafa Ali Azmi <syafaali23@gmail.com>
2025-12-05 23:51:35 +0700

    add edit app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import io
 import os
 from PIL import Image
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 # --- Konfigurasi Model ---
 MODEL_PATH = 'model/model_vgg16.keras'
@@ -123,3 +123,15 @@ def classify():
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
+    
+    
+#     from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def home():
+#     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', debug=True)
